c6aca7f82fd126d877c3e514e874d037446a8390
Maxim Hordijchuk <uzr.snikers.ua@gmail.com>
2013-12-12 19:13:39 +0200

    Project started!

---
+++ Lab3.XML/DataBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Lab3.XML
+{
+    class DataBase
+    {
+        private XmlDocument _studentDatabase;
+        private MainForm _mainForm;
+        private const string FilePath = @"d:\Projects\UnivProjects\Lab3.XML\Lab3.XML\Base.xml";
+
+        public DataBase(MainForm mainForm)
+        {
+            this._mainForm = mainForm;
+            LoadXmlDocument(FilePath);
+        }
+
+        void LoadXmlDocument(string path)
+        {
+            if (_studentDatabase == null)
+                _studentDatabase = new XmlDocument();
+            _studentDatabase.Load(path);
+        }
+
+        public string GetNodes()
+        {
+            return GetNodes(_studentDatabase.DocumentElement);
+        }
+
+        string GetNodes(XmlNode node)
+        {
+            var result = new StringBuilder();
+            GetNodes(node, 0, result);
+            return result.ToString();
+        }
+
+        void GetNodes(XmlNode node, int level, StringBuilder result)
+        {
+            result.Append(node.Value);
+            foreach (XmlNode n in node.ChildNodes)
+            {
+                GetNodes(n, level + 1, result);
+            }
+        }
+
+        public void CreateNode()
+        {
+            XmlNode node = _studentDatabase.CreateNode(XmlNodeType.Text, "TextNode", null);
+            
+            _studentDatabase.Save("newXML.xml");
+        }
+    }
+}
---
+++ Lab3.XML/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3.XML
+{
+    public partial class MainForm : Form
+    {
+        private DataBase dataBase;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            dataBase = new DataBase(this);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result = dataBase.GetNodes();
+            richTextBox1.Clear();
+            richTextBox1.AppendText(result);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataBase.CreateNode();
+        }
+    }
+}
